e2c3b0fc0a3867a31c4f295b48ee5fc9b2eb43c0
James Wilson <testingonlive@gmail.com>
2015-02-06 20:30:18 +0000

    first commit of project8

---
+++ project8.js
@@ -0,0 +1,44 @@
+// Project 8 - Digital Hourglass
+
+// @TODO clean up JS
+// @TODO countdown after tilt instead of reset
+
+var five = require( 'johnny-five' ),
+    board = new five.Board();
+
+board.on( 'ready', function(){
+
+    var tilt = new five.Sensor.Digital( 8 ),
+        // seems like a sensible way to set the pin No        
+        ledArr = [ 2, 3, 4, 5, 6, 7 ], 
+        count = 0;
+
+    // overwrite pin no with LED obj   
+    ledArr.forEach( function( elm, ind, arr ){
+        arr[ ind ] = new five.Led( elm );
+    }); 
+
+    
+    // might replcae this with setInterval    
+    ( function _loop(){
+        setTimeout( function(){
+            if ( count < ledArr.length ) ledArr[ count++ ].on();
+            _loop();
+
+        }, 5000);
+
+    }())
+
+    // if the tilt changes, reset    
+    tilt.on( 'change', function(){
+        console.log( this.value );
+        ledArr.forEach( function( elm ){
+            elm.off();
+        });
+
+        count = 0;
+    })
+    
+    
+
+});
