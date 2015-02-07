123b9115c5ee5faa3a77857368ab4e90130d2409
testingonlive <testingonlive@gmail.com>
2015-02-07 20:28:49 +0000

    countdown now as well as countup

--- project8.js
+++ project8.js
@@ -1,8 +1,5 @@
 // Project 8 - Digital Hourglass
 
-// @TODO clean up JS
-// @TODO countdown after tilt instead of reset
-
 var five = require( 'johnny-five' ),
     board = new five.Board();
 
@@ -11,6 +8,9 @@ board.on( 'ready', function(){
     var tilt = new five.Sensor.Digital( 8 ),
         // seems like a sensible way to set the pin No        
         ledArr = [ 2, 3, 4, 5, 6, 7 ],
+		ledArrLen = ledArr.length,
+		// tilt change fires onready, hence -1		
+		dir = -1,
         count = 0;
 
     // overwrite pin no with LED obj   
@@ -21,21 +21,41 @@ board.on( 'ready', function(){
     
     // might replcae this with setInterval        
     setInterval( function(){
-        if ( count < ledArr.length ) ledArr[ count++ ].on();
-        
+		// bit of logging		
+		console.log( count + ' - ' + dir );
 
-    }, 2000);
+		// depending on the direction, off or on	
+		~( Math.abs( dir ) / dir ) ? ledArr[ count ].on() : ledArr[ count ].off();
+		
+		// id increamenting count will result in a valid value do it
+		if ( count + dir >= 0 && count + dir < ledArrLen ) count = count + dir;
 
-    
+    }, 2e3 );
+
+    // throttle the tilt event a bit
+	var tilted = (function(){
+		var _tilted = false;
+		
+		return function ( no ){
+			
+			if ( !_tilted ) {
+				setTimeout(function(){
+					_tilted = false;					
+				}, no )
+
+				return _tilted = true;
+			} else {
+				return false;
+			}
+
+		}
+
+	}());
 	
     // if the tilt changes, reset    
-    tilt.on( 'change', function(){
-        console.log( this.value );
-        ledArr.forEach( function( elm ){
-            elm.off();
-        });
-
-        count = 0;
+    tilt.on( 'change', function(){		
+		// second seems like a good value to start with        
+		if ( tilted( 1e3 ) ) dir *= -1;
     })
     
     
