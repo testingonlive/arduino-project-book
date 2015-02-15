7bbb42d3071e526444198369224caa012fda938c
testingonlive <testingonlive@gmail.com>
2015-02-15 12:29:53 +0000

    control motor speed with pot

--- project9.js
+++ project9.js
@@ -1,5 +1,4 @@
 // Project 9 - Motorized Pinwheel
-// @TODO use potentiometer to vary speed
 
 var five = require( 'johnny-five' ),
 	board = new five.Board();
@@ -7,15 +6,27 @@ var five = require( 'johnny-five' ),
 board.on( 'ready', function(){
 
 	var button = new five.Button( 2 ),
-		motor = new five.Motor( 9 );
+		motor = new five.Motor( 9 ),
+		pot = new five.Sensor( 'A0' ),
+		// check if button is pressed		
+		on = false;
+
+	pot.scale( [ 0, 255 ] ).on( 'data', function(){
+		var speed = +this.value;
+		console.log( 'scaled value - ' + speed.toFixed( 2 ) );		
+				
+		motor.stop();
+
+		if ( on ) motor.start( speed );
+		
+	});	
 
 	button.on( 'press', function(){
-		// bit noisy at full speed		
-		motor.start( 50 );	
+		on = true;
 	});
 
 	button.on( 'release', function(){
-		motor.stop();
+		on = false;
 	});
 
 
